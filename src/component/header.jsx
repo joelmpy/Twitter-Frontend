bc1831a674136efbb6183567a903ae357f0fc959
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-05-11 17:27:55 +0200

    Starting profil

---
+++ src/component/header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBrands, faTwitter } from '@fortawesome/free-solid-svg-icons'
+
+
+function Header() {
+    return (
+        <div >
+            <aside className="col-2 pt-2 bg-dark">
+                <header className="bg-dark">
+                    <ul className="flex-column">
+                        {/* <FontAwesomeIcon icon="fa-brands fa-twitter" id="twitter-icon" /> */}
+                        <li className="nav-item">
+                            <a className="nav-link pl-0 text-light" href="#">Acceuil</a>
+                            <a className="nav-link pl-0 text-light" href="#">Profil</a>
+                        </li>
+                        <Button id="button-tweeter" variant="info">Tweeter</Button>
+                    </ul>
+                </header>
+            </aside>
+        </div>
+    )
+}
+
+export default Header
+
+
+
+
+
+
+
+
+
--- src/routes/Routeur.jsx
+++ src/routes/Routeur.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Login from "../pages/login";
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom";
 import Firstpage from "../pages/firstpage";
+import Profil from "../pages/profil"
 
 function Routeur ()  {
     return (
         <Routes>
             <Route path="/" element={<Firstpage/>}></Route>
             <Route path="/Login" element={<Login/>}></Route>
+            <Route path="/profil" element={<Profil/>}></Route>
         </Routes>
     )
 }
