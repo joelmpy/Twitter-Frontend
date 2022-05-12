da9c8b9cbf30fd95c4d235fd802215e1c5b65ac8
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-05-12 16:59:17 +0200

    correcting conflict

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import Router from "./routes/Router";
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="App">
       <Routeur />
-=======
-    <div className="container App">
-      <Router />
->>>>>>> 2dc65a4f9b5ffaf06cdcb7abb3f63ec28bc07364
     </div>
   );
 }
